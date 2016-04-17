ca9eef2c37065df2f0444e37cc94af4ea288c58b
Francois Hill (home win10) <francoishill11@gmail.com>
2016-04-17 08:01:42 +0200

    Initial commit with Windows, Linux, Darwin and example.

---
+++ os_visitors.go
@@ -0,0 +1,25 @@
+package osvisitors
+
+var (
+	WindowsOs OsType = &windows{}
+	LinuxOs   OsType = &linux{}
+	DarwinOs  OsType = &darwin{}
+)
+
+type OsType interface {
+	Accept(v OsTypeVisitor)
+}
+
+type OsTypeVisitor interface {
+	VisitWindows()
+	VisitLinux()
+	VisitDarwin()
+}
+
+type windows struct{}
+type linux struct{}
+type darwin struct{}
+
+func (*windows) Accept(v OsTypeVisitor) { v.VisitWindows() }
+func (*linux) Accept(v OsTypeVisitor)   { v.VisitLinux() }
+func (*darwin) Accept(v OsTypeVisitor)  { v.VisitDarwin() }
